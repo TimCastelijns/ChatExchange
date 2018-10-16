db9033186d1d3da12294f904cfc6b9729ba11f43
Tim Castelijns <timcastelijns@gmail.com>
2018-10-16 12:20:21 +0200

    Tests for ChatHost

---
+++ src/test/kotlin/com/timcastelijns/chatexchange/chat/ChatHostTest.kt
@@ -0,0 +1,28 @@
+package com.timcastelijns.chatexchange.chat
+
+import com.timcastelijns.chatexchange.chat.ChatHost
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ChatHostTest {
+
+    @Test
+    fun `stack overflow baseurl should be correct`() {
+        val expected = "https://chat.stackoverflow.com"
+        assertEquals(expected, ChatHost.STACK_OVERFLOW.baseUrl)
+    }
+
+    @Test
+    fun `stack exchange baseurl should be correct`() {
+        val expected = "https://chat.stackexchange.com"
+        assertEquals(expected, ChatHost.STACK_EXCHANGE.baseUrl)
+    }
+
+    @Test
+    fun `meta stack exchange baseurl should be correct`() {
+        val expected = "https://chat.meta.stackexchange.com"
+        assertEquals(expected, ChatHost.META_STACK_EXCHANGE.baseUrl)
+    }
+
+}
+
